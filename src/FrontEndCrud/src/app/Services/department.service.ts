0bca96d3b1b268f64182c46faf17ee82f767e3e3
Ameya Shinde <ameyashinde123@outlook.com>
2022-11-23 03:05:11 +0530

    Added services methods.

--- src/FrontEndCrud/src/app/Services/department.service.ts
+++ src/FrontEndCrud/src/app/Services/department.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseAPI } from '../Interfaces/response-api';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DepartmentService {
 
-  constructor() { }
+    private endpoint: string = environment.endpoint;
+    private myApiUrl: string = this.endpoint + "api/department";
+
+    constructor(private http: HttpClient) { }
+
+    getList(): Observable<ResponseAPI> {
+        return this.http.get<ResponseAPI>(this.myApiUrl);
+    }
 }
--- src/FrontEndCrud/src/app/Services/employee.service.ts
+++ src/FrontEndCrud/src/app/Services/employee.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Employee } from '../Interfaces/employee';
+import { ResponseAPI } from '../Interfaces/response-api';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+    private endpoint: string = environment.endpoint;
+    private myApiUrl: string = this.endpoint + "api/employee";
+
+    constructor(private http: HttpClient) { }
+
+    getList(): Observable<ResponseAPI> {
+        return this.http.get<ResponseAPI>(this.myApiUrl);
+    }
+
+    add(request: Employee): Observable<ResponseAPI> {
+        return this.http.post<ResponseAPI>(this.myApiUrl, request);
+    }
+
+    update(request: Employee): Observable<ResponseAPI> {
+        return this.http.put<ResponseAPI>(this.myApiUrl, request);
+    }
+
+    delete(id: number): Observable<ResponseAPI> {
+        return this.http.delete<ResponseAPI>(`${this.myApiUrl}/${id}`);
+    }
 }
